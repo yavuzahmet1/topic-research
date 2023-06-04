eed2c3f67f84b0dbdbd54183f4442bb03df644d5
Coders club <1yavuzahmet@gmail.com>
2023-06-05 00:40:02 +0300

    HashMap

---
+++ map-hashmap-treemap/map-hashmap-treemap/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Map<String, Product> productsByName = new HashMap<>();
+        Product eBike = new Product("E-Bike", "A bike with a battery");
+        Product roadBike = new Product("Road bike", "A bike for competition");
+        productsByName.put(eBike.getName(), eBike);
+        productsByName.put(roadBike.getName(), roadBike);
+
+        // Create a HashMap object called capitalCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+    }
+    }
