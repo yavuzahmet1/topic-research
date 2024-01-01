a2f0c815aa4925d0db784244c9ee00c3c9db4c53
Coders club <1yavuzahmet@gmail.com>
2024-01-01 16:04:17 +0300

    mongodb totorial

---
+++ mongodb-tutorial/src/main/java/com/mongodb/tutorial/mongodbtutorial/MongodbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.tutorial.mongodbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbTutorialApplication.class, args);
+	}
+
+}
---
+++ mongodb-tutorial/src/test/java/com/mongodb/tutorial/mongodbtutorial/MongodbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.tutorial.mongodbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
